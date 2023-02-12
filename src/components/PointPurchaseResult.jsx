9a102fcb38a03a0cb8f5f6b657b21ad0bb8e6a50
heyjyu <jiwoosmile@yonsei.ac.kr>
2023-02-12 14:54:10 +0900

    구매 성공 페이지 스타일링
    구매 성공 페이지에 디자인을 입혔다.

--- src/components/PointPurchaseResult.jsx
+++ src/components/PointPurchaseResult.jsx
@@ -1,7 +1,46 @@
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 import usePointStore from '../hooks/usePointStore';
 import Header from './ui/Header';
 import Title from './ui/Title';
+
+const Container = styled.div`
+  background: white;
+  padding: 2em 1em;
+  border: 1px solid #EAEAEC;
+
+  h2 {
+    font-size: 1.2em;
+    font-weight: 700;
+    margin-bottom: 2em;
+  }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  margin-block: 1em;
+
+  p:first-child {
+    font-weight: 700;
+    width: 10em;
+  }
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Button = styled.button`
+  font-weight: 700;
+  width: 10em;
+  height: 3em;
+  padding: 0.5em 3em;
+  border: 1px solid #AB92FF;
+  border-radius: 0.25em;
+  background: #BAA5FF;
+  color: white;
+`;
 
 export default function PointPurchaseResult() {
   const navigate = useNavigate();
@@ -19,24 +58,36 @@ export default function PointPurchaseResult() {
           구매 결과
         </Title>
       </Header>
-      <p>구매가 정상적으로 완료되었습니다</p>
-      <p>구매일시</p>
-      <p>{paymentResult.approved_at}</p>
-      <p>주문번호</p>
-      <p>{paymentResult.partner_order_id}</p>
-      <p>구매포인트</p>
-      <p>
-        {paymentResult.quantity}
-        pt
-      </p>
-      <p>결제금액</p>
-      <p>
-        {paymentResult.amount.total.toLocaleString()}
-        원
-      </p>
-      <button type="button" onClick={() => navigate('/mypage')}>
-        확인
-      </button>
+      <Container>
+        <h2>구매가 정상적으로 완료되었습니다</h2>
+        <Wrapper>
+          <p>구매일시</p>
+          <p>{paymentResult.approved_at}</p>
+        </Wrapper>
+        <Wrapper>
+          <p>주문번호</p>
+          <p>{paymentResult.partner_order_id}</p>
+        </Wrapper>
+        <Wrapper>
+          <p>구매포인트</p>
+          <p>
+            {paymentResult.quantity}
+            pt
+          </p>
+        </Wrapper>
+        <Wrapper>
+          <p>결제금액</p>
+          <p>
+            {paymentResult.amount.total.toLocaleString()}
+            원
+          </p>
+        </Wrapper>
+        <ButtonWrapper>
+          <Button type="button" onClick={() => navigate('/mypage')}>
+            확인
+          </Button>
+        </ButtonWrapper>
+      </Container>
     </div>
   );
 }
